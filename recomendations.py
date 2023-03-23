b057ca999c7b36c26ce7bd26548f025b8b93d0a3
Daniele Simas <83620485+danisimas@users.noreply.github.com>
2023-03-23 08:24:10 -0400

    Update recomendations.py

--- recomendations.py
+++ recomendations.py
@@ -18,6 +18,11 @@ def recomendar_itens(latitude, longitude):
 
     return df.head(20)
 
+def max_similarity(product_list, target_word):
+    products = product_list.split(", ")
+    similarities = [fuzz.token_sort_ratio(target_word, product) for product in products]
+    return max(similarities)
+
 def recomendar_itens_com_palavra(latitude, longitude, palavra):
     latitude = float(latitude)
     longitude = float(longitude)
@@ -30,8 +35,8 @@ def recomendar_itens_com_palavra(latitude, longitude, palavra):
     df.dropna(subset=['Latitude', 'Longitude'], inplace=True)
 
     df['distancia'] = df.apply(lambda row: distance.distance(user_location, (row['Latitude'], row['Longitude'])).m, axis=1)
-    df['similaridade'] = df['Lista de Produtos'].apply(lambda x: fuzz.token_sort_ratio(palavra, x))
-    df['score'] = (1 - df['distancia'] / df['distancia'].max()) * 0.5 + df['similaridade'] / 100 * 0.5
+    df['similaridade'] = df['Lista de Produtos'].apply(lambda x: max_similarity(x, palavra))
+    df['score'] = (1 - df['distancia'] / df['distancia'].max()) * 0.25 + df['similaridade'] / 100 * 0.75
     df.sort_values(by='score', ascending=False, inplace=True)
-
+    print(df['score'])
     return df.head(20)
